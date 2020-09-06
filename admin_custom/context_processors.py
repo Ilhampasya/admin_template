adf2fa205b8341cf3315cb0874dfa2a92c7e0f9a
Ilham Pasya <ilhampasya920@gmail.com>
2020-09-06 18:03:12 +0700

    Move app_list to app_lists

--- admin_custom/context_processors.py
+++ admin_custom/context_processors.py
@@ -6,4 +6,12 @@ from django.utils.text import capfirst
 site = admin.site
 
 def applist(request):
-	return {'app_list': admin.site.get_app_list(request)}
+    from django.conf import settings
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.utils.text import capfirst
+
+site = admin.site
+
+def applist(request):
+	return {'app_lists': site.get_app_list(request)}
